baef023934374132d304ba22ba7e1552d7271ef0
Sarah Hong <ssh2198@columbia.edu>
2024-01-11 23:57:15 -0500

    final regression

--- part1/PCSL_reg.R
+++ part1/PCSL_reg.R
@@ -42,10 +42,36 @@ mono_pca <- princomp(mono_cormat, cor=TRUE, scores=TRUE)
 summary(cat_pca)
 summary(mono_pca)
 
-# Catamaran visualizations
+# Catamaran PCA visualizations
 fviz_cos2(cat_pca, choice = "var", axes = 1:2) # Cos2
 fviz_eig(cat_pca) # Scree
+fviz_pca_biplot(cat_pca) # Biplot
 
-# Monohull visualizations
+# Monohull PCA visualizations
 fviz_cos2(mono_pca, choice = "var", axes = 1:2) # Cos2
 fviz_eig(mono_pca) # Scree
+fviz_pca_biplot(mono_pca) # Biplot
+
+# Select only top 2 components
+cat_pcs <- predict(cat_pca, newdata = num_cat)[, 1:2]
+cat_pcs_data <- as.data.frame(cbind(price=gdp_cat_df$price, cat_pcs))
+mono_pcs <- predict(mono_pca, newdata = num_mono)[, 1:2]
+mono_pcs_data <- as.data.frame(cbind(price=gdp_mono_df$price, mono_pcs))
+
+# PCA regression models
+cat_pcs_model <- lm(price ~ Comp.1 + Comp.2, data = cat_pcs_data)
+mono_pcs_model <- lm(price ~ Comp.1 + Comp.2, data = mono_pcs_data)
+# Print summary statistics
+summary(cat_pcs_model)
+summary(mono_pcs_model)
+
+# Non-PCA regression models (these yielded higher R^2 values)
+num_cat2 <- gdp_cat_df[, c('price', 'length', 'year', 'gdp')]
+num_mono2 <- gdp_mono_df[, c('price', 'length', 'year', 'gdp')]
+
+cat_raw_model <- lm(price ~ length+year+gdp, data=num_cat2)
+mono_raw_model <- lm(price ~ length+year+gdp, data=num_mono2)
+
+summary(cat_raw_model)
+summary(mono_raw_model)
+
