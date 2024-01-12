a26a7dc0393c9bed29a25d75f242b6811f27a4bf
Sarah Hong <ssh2198@columbia.edu>
2024-01-11 22:35:38 -0500

    Merge anova changes

---
+++ part1/anova.R
@@ -0,0 +1,13 @@
+# Functions to calculate ANOVA
+
+# Get one-way ANOVA table
+m.aov1 <- aov(price ~ small_region, data = cleaned_monohull_data)
+summary(m.aov1)
+c.aov1 <- aov(price ~ small_region, data = cleaned_catamaran_data)
+summary(c.aov1)
+
+# Get two-way ANOVA table
+m.aov2 <- aov(price ~ small_region + variant, data = cleaned_monohull_data)
+summary(m.aov2)
+c.aov2 <- aov(price ~ small_region + variant, data = cleaned_catamaran_data)
+summary(c.aov2)
--- part1/load_raw.R
+++ part1/load_raw.R
@@ -102,15 +102,3 @@ cleaned_catamaran_data <- catamaran_data %>%
 mono_names <- unique(paste(cleaned_monohull_data$make, cleaned_monohull_data$variant))
 cat_names <- unique(paste(cleaned_catamaran_data$make, cleaned_catamaran_data$variant))
 
-# Get one-way ANOVA table
-m.aov1 <- aov(price ~ small_region, data = cleaned_monohull_data)
-summary(m.aov1)
-c.aov1 <- aov(price ~ small_region, data = cleaned_catamaran_data)
-summary(c.aov1)
-
-# Get two-way ANOVA table
-m.aov2 <- aov(price ~ small_region + variant, data = cleaned_monohull_data)
-summary(m.aov2)
-c.aov2 <- aov(price ~ small_region + variant, data = cleaned_catamaran_data)
-summary(c.aov2)
-
