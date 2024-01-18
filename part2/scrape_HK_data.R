30a82f43a8bef032f8b8446ce131e2619ce70b58
Sarah Hong <ssh2198@columbia.edu>
2024-01-17 21:41:20 -0500

    Scraped hk data

--- part2/scrape_HK_data.R
+++ part2/scrape_HK_data.R
@@ -6,50 +6,41 @@ library(httr)
 library(rvest)
 
 # Returns a dataframe with sailboat attribute data
-scrape_page <- function(url) {
+scrape_page <- function() {
   # Read in html of webpage
   url <- 'https://www.yachtworld.com/boats-for-sale/country-hong-kong/'
 
-  webpage <- read_html('https://theboater.hk/sail/')
-
+  #webpage <- read_html('https://theboater.hk/sail/')
+  webpage <- read_html('https://www.rightboat.com/boats-for-sale/HK')
 
   # Get specific html elements
-  sail_names <- webpage %>%
-    html_nodes("h4") %>%
+  sailboats <- webpage %>%
+    html_elements("div.flex.flex-col.justify-between.flex-1.h-full")
+
+  sail_names <- sailboats %>%
+    html_elements('h2') %>%
+    html_text2()
+  sail_details <- sailboats %>%
+    html_elements('p.font-bold.text-gray-raisin-black.text-body-3') %>%
+    html_text2()
+  sail_prices <- sailboats %>%
+    html_elements('p.py-1.font-bold.text-body-2') %>%
     html_text2()
 
-  sail_price <- webpage %>%
-    html_nodes("div.price") %>%
-    html_text2()
+  headers <- c('name', 'condition', 'length', 'year', 'seller')
+  df <- data.frame(matrix(NA, nrow=length(sail_names), ncol=length(headers)))
+  colnames(df) <- headers
 
-  sail_details <- webpage %>%
-    html_nodes("div.details") %>%
-    html_text2()
-
-  # Extract unique headers
-  unique_headers <- c('name', unique(sail_headers))
-
-  # Initialize an empty dataframe with columns named after unique headers
-  df <- data.frame(matrix(NA, nrow=length(sail_names), ncol=length(unique_headers)))
-  colnames(df) <- unique_headers
-
-  # Assign sail names to the 'name' column
   df$name <- sail_names
-
-  # Iterate through sail_meta and populate the dataframe
-  current_row <- 1
-  for (j in seq_along(sail_meta)) {
-    # Extract metadata and assign to the corresponding column
-    header <- sail_headers[j]
-    value <- sail_meta[j]
-    df[current_row, header] <- value
-
-    # Find the starting index of the next sailboat's metadata
-    if (grepl("^\\$", sail_meta[j])) {
-      current_row <- current_row + 1
+  count <- 1
+  for (i in 1:nrow(df)) {
+    for (j in 2:5) {
+      df[i, j] <- sail_details[count]
+      count <- count + 1
     }
   }
-  names(df) <- c('name', 'length', 'beam', 'draft', 'year', 'type', 'hull', 'engine', 'location', 'price')
+
+  df$price <- sail_prices
   return(df)
 }
 
