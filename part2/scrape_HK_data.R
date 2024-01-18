7bdb44648de3c6e90f20b8738a57619e46a5a1a7
Sarah Hong <ssh2198@columbia.edu>
2024-01-17 22:05:22 -0500

    Wrote hk data to file

--- part2/scrape_HK_data.R
+++ part2/scrape_HK_data.R
@@ -6,42 +6,62 @@ library(httr)
 library(rvest)
 
 # Returns a dataframe with sailboat attribute data
-scrape_page <- function() {
+scrape_rightboat <- function() {
   # Read in html of webpage
-  url <- 'https://www.yachtworld.com/boats-for-sale/country-hong-kong/'
+  #url <- 'https://www.yachtworld.com/boats-for-sale/country-hong-kong/'
 
+  headers <- c('name', 'condition', 'length', 'year', 'seller', 'price')
+  big_df <- data.frame(matrix(NA, nrow=1, ncol=length(headers)))
+  colnames(big_df) <- headers
   #webpage <- read_html('https://theboater.hk/sail/')
-  webpage <- read_html('https://www.rightboat.com/boats-for-sale/HK')
 
-  # Get specific html elements
-  sailboats <- webpage %>%
-    html_elements("div.flex.flex-col.justify-between.flex-1.h-full")
+  for (i in 1:3) {
+    link <- paste0('https://www.rightboat.com/boats-for-sale/HK?page=', i)
+    webpage <- read_html(link)
 
-  sail_names <- sailboats %>%
-    html_elements('h2') %>%
-    html_text2()
-  sail_details <- sailboats %>%
-    html_elements('p.font-bold.text-gray-raisin-black.text-body-3') %>%
-    html_text2()
-  sail_prices <- sailboats %>%
-    html_elements('p.py-1.font-bold.text-body-2') %>%
-    html_text2()
+    # Get specific html elements
+    sailboats <- webpage %>%
+      html_elements("div.flex.flex-col.justify-between.flex-1.h-full")
 
-  headers <- c('name', 'condition', 'length', 'year', 'seller')
-  df <- data.frame(matrix(NA, nrow=length(sail_names), ncol=length(headers)))
-  colnames(df) <- headers
+    sail_names <- sailboats %>%
+      html_elements('h2') %>%
+      html_text2()
+    sail_details <- sailboats %>%
+      html_elements('p.font-bold.text-gray-raisin-black.text-body-3') %>%
+      html_text2()
+    sail_prices <- sailboats %>%
+      html_elements('p.py-1.font-bold.text-body-2') %>%
+      html_text2()
 
-  df$name <- sail_names
-  count <- 1
-  for (i in 1:nrow(df)) {
-    for (j in 2:5) {
-      df[i, j] <- sail_details[count]
-      count <- count + 1
+
+    df <- data.frame(matrix(NA, nrow=length(sail_names), ncol=length(headers)))
+    colnames(df) <- headers
+
+    df$name <- sail_names
+    count <- 1
+    for (i in 1:nrow(df)) {
+      for (j in 2:5) {
+        df[i, j] <- sail_details[count]
+        count <- count + 1
+      }
     }
+
+    df$price <- sail_prices
+    big_df <- rbind(big_df, df)
   }
 
-  df$price <- sail_prices
-  return(df)
+  big_df$length <- gsub(" ft.", "", big_df$length)
+  big_df$price <- as.numeric(gsub("[^0-9]", "", big_df$price))  # Remove non-numeric characters
+  big_df$price[big_df$price == 0] <- NA
+  big_df <- na.omit(big_df)
+
+  write.csv(big_df, './part2/data/hk_data.csv')
+
+  return(big_df)
+}
+
+scrape_theboater <- function() {
+  vc_gitem-zone-mini
 }
 
 # Function to generate URLs for multiple pages
